56ad5c8d719e119ead9c48c67eac31f726bee1fb
WebBreacher <webbreacher@gmail.com>
2012-10-25 12:41:29 -0400

    Adding files for publication

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 kxp
-===
-
-Kismet XML Parser
+====
+This is a Kismet Net XML parser to pull out the BSSIDs of certain SSIDs for inclusion in the filter option within kismet.conf<br>
---
+++ kxp.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python -tt
+'''
+-------------------------------------------------------------------------------
+Name:        KXP - Kismet XML Parser
+Purpose:     Extract the proper BSSIDs from a Kismet .netxml file given an SSID
+Author:      Micah Hoffman (@WebBreacher)
+-------------------------------------------------------------------------------
+'''
+
+import os, sys
+import xml.etree.ElementTree as ET
+
+
+#=================================================
+# Constants and Variables
+#=================================================
+filter_out = []
+
+#=================================================
+# Functions & Classes
+#=================================================
+
+# The entire bcolors class was taken verbatim from the Social Engineer's Toolkit
+# check operating system
+def check_os():
+    if os.name == "nt":
+        operating_system = "windows"
+    if os.name == "posix":
+        operating_system = "posix"
+    return operating_system
+
+#
+# Class for colors
+#
+if check_os() == "posix":
+    class bcolors:
+        PURPLE = '\033[95m'
+        CYAN = '\033[96m'
+        DARKCYAN = '\033[36m'
+        BLUE = '\033[94m'
+        GREEN = '\033[92m'
+        YELLOW = '\033[93m'
+        RED = '\033[91m'
+        ENDC = '\033[0m'
+
+        def disable(self):
+            self.PURPLE = ''
+            self.CYAN = ''
+            self.BLUE = ''
+            self.GREEN = ''
+            self.YELLOW = ''
+            self.RED = ''
+            self.ENDC = ''
+
+# if we are windows or something like that then define colors as nothing
+else:
+    class bcolors:
+        PURPLE = ''
+        CYAN = ''
+        DARKCYAN = ''
+        BLUE = ''
+        GREEN = ''
+        YELLOW = ''
+        RED = ''
+        ENDC = ''
+
+        def disable(self):
+            self.PURPLE = ''
+            self.CYAN = ''
+            self.BLUE = ''
+            self.GREEN = ''
+            self.YELLOW = ''
+            self.RED = ''
+            self.ENDC = ''
+
+def main():
+
+    print bcolors.GREEN + "\n[Start] " + bcolors.CYAN + "Starting the KXP script. Hang on."
+
+    # Check how many command line args were passed and provide HELP msg if not right
+    if len(sys.argv) == 2:
+        kismet_log_file=sys.argv[1]
+    else:
+        print bcolors.RED + "[Error] " + bcolors.ENDC + "You need to enter in the logfile and name such as: %s [logfilename]\n" % sys.argv[0]
+        sys.exit()
+
+    # Open the kismet_log_file (or try to)
+    try:
+        tree = ET.parse(kismet_log_file)
+
+    except (IOError) :
+        print bcolors.RED + "[Error] " + bcolors.ENDC + "Can't read file the logfile you entered."
+        sys.exit()
+
+    print bcolors.GREEN + "[Info] " + bcolors.CYAN + "Kismet Net XML file successfully read."
+
+    # Enter in the SSIDs as a comma sep list
+    print bcolors.YELLOW + "[Input]" + bcolors.ENDC + " the names of the SSIDs you want the BSSIDs for. If multiple, separate with commas. "
+    target_ssids = sys.stdin.readline()
+    target_ssid = target_ssids.split(",")
+
+    root = tree.getroot()
+          
+    # Cycle through the XML doc looking for the targets
+    for target in target_ssid:
+        target = target.strip()
+        target_counter = 0
+        print bcolors.BLUE + "[Searching] " + bcolors.ENDC + "Now searching for " + bcolors.GREEN + "%s" % target
+
+        for wireless_net in root.findall('wireless-network'):
+            for ssid in wireless_net.findall('SSID'):
+                essid = ssid.find('essid').text
+                if essid == target:
+                    target_counter += 1
+                    bssid = wireless_net.find('BSSID').text
+                    filter_out.append(bssid)
+        print bcolors.YELLOW + "[Status] " + bcolors.ENDC + "Found %d networks with '%s' name." % (target_counter, target)
+        
+    print bcolors.GREEN + "[Finished] " + bcolors.ENDC + "Found %d total networks to filter. They are below.\n" % len(filter_out)
+    
+    # Kismet config file content
+    kismet_string = 'filter_tracker=ANY(!"' + '","'.join(filter_out) + '")'
+    print kismet_string
+
+
+#=================================================
+# START
+#=================================================
+
+if __name__ == "__main__": main()
